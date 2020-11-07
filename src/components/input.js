d9ed6bb3631663f43c06048546a0cb1bd75afd22
Daniel Soares <daniel.soares@me.com>
2020-11-07 06:07:53 -0300

    💄 Normalize inputs

--- src/components/input.js
+++ src/components/input.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Input = ({ ...props }) => (
   <input
-    className="block border-black border-solid border w-full p-2"
+    className="block border-black border-solid border rounded-none bg-transparent w-full p-2"
     {...props}
   />
 )
--- src/components/text-area.js
+++ src/components/text-area.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TextArea = ({ ...props }) => (
   <textarea
-    className="block border-black border-solid border w-full h-40 p-2 resize-none"
+    className="block border-black border-solid border rounded-none bg-transparent w-full h-40 p-2 resize-none"
     {...props}
   />
 )
