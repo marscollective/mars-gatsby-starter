1c513bc71dd28a53a09608b3f9f64241081fa106
Daniel Soares <daniel.soares@me.com>
2020-11-06 22:02:07 -0300

    🎨 Improve Image component

--- src/components/image.js
+++ src/components/image.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import Img from 'gatsby-image'
+import BackgroundImage from 'gatsby-background-image'
 
-const Image = ({ alt, className, image }) =>
+export const Image = ({ alt, className, image }) =>
   !!image?.childImageSharp ? (
     <Img fluid={image.childImageSharp.fluid} alt={alt} className={className} />
   ) : (
-    <div className={className} style={{ overflow: 'hidden' }}>
+    <div className={`${className} overflow-hidden`}>
       <img
         src={image}
-        style={{
-          width: '100%',
-          height: '100%',
-          objectFit: 'cover',
-          objectPosition: 'center center'
-        }}
         alt={alt}
+        className="object-cover object-center w-full h-full"
       />
     </div>
   )
 
-export default Image
+export const Background = ({ className, children, image }) =>
+  !!image?.childImageSharp ? (
+    <BackgroundImage fluid={image.childImageSharp.fluid} className={className}>
+      {children}
+    </BackgroundImage>
+  ) : (
+    <div
+      className={`${className} bg-cover bg-center`}
+      style={{
+        backgroundImage: `url(${image})`
+      }}
+    >
+      {children}
+    </div>
+  )
