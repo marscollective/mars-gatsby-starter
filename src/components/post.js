d4bbf448f5f1491a0b38c863de126c61dbdb857d
Daniel Soares <daniel.soares@me.com>
2020-11-23 13:24:21 -0300

    💄 Improve post layout

--- src/components/post.js
+++ src/components/post.js
@@ -2,18 +2,22 @@ import React from 'react'
 import { Background } from '@components/image'
 
 const Post = ({ body, date, image, timeToRead, title }) => (
-  <div className="container">
-    <Background src={image} className="mb-16">
-      <div className="grip gap-4 px-4 py-48 text-center text-white bg-gradient-to-t from-black to-transparent">
-        <span className="text-sm">
-          {date && `${date} - ${timeToRead} minutes of reading`}
-        </span>
-        <h1 className="m-0">{title}</h1>
+  <article>
+    <Background src={image}>
+      <div className="bg-gradient-to-t from-black to-transparent">
+        <div className="container my-0 py-48 text-center text-white">
+          <h1 className="m-0 mb-4">{title}</h1>
+          <span className="text-sm">
+            {date && `${date} - ${timeToRead} min. of reading`}
+          </span>
+        </div>
       </div>
     </Background>
 
-    <div className="md:mx-16 lg:mx-40">{body}</div>
-  </div>
+    <div className="container">
+      <div className="md:mx-16 lg:mx-40">{body}</div>
+    </div>
+  </article>
 )
 
 export default Post
